444e943840c66301eb7ae80d1e0f1ba7b21b37ef
Pawe Kedzia <pawel.kedzia95@gmail.com>
2018-03-03 12:09:33 +0100

    connect to devtools

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import reducers from './src/reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore(
+	reducers,
+	composeWithDevTools(applyMiddleware())
+  );
 
 export default class App extends React.Component {
 		render() {
 				return (
-						<Provider store={createStoreWithMiddleware(reducers)}>
+						<Provider store={store}>
 								<View>
 										<Text>joł joł team 2</Text>
 								</View>
