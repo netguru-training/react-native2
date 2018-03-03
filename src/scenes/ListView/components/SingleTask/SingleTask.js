304ee2cbbc11c96202397066b6e909a2705047a2
Patryk <patryk.dom15@gmail.com>
2018-03-03 13:05:59 +0100

    Adding single task component

--- src/scenes/ListView/components/SingleTask/SingleTask.js
+++ src/scenes/ListView/components/SingleTask/SingleTask.js
@@ -1,3 +1,44 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import Swipeout from 'react-native-swipeout';
+import {View, Text, Icon, Button} from "native-base"
 
 import styles from "./styles";
+
+import TaskButton from "../TaskButton/TaskButton";
+
+const rightSwipeoutBtns = [
+  {
+    component: <TaskButton
+      text="Delete"
+      icon="trash"
+      onPress={() => console.log("delete")}
+      danger
+    />,
+  }
+];
+
+const leftSwipeOutBtns = [
+  {
+    component: <TaskButton
+      text="Done"
+      icon="checkmark"
+      onPress={() => console.log("check")}
+      success
+    />,
+  }
+];
+
+const SingleTask = ({ text }) => (
+  <Swipeout right={rightSwipeoutBtns} left={leftSwipeOutBtns} buttonWidth={100}>
+    <View style={styles.container}>
+      <Text>{text}</Text>
+    </View>
+  </Swipeout>
+);
+
+SingleTask.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default SingleTask;
